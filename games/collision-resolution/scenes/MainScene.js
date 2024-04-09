05823da14dc1cab06206f6ce91200c180247ae92
B. Ricks <bricks@unomaha.edu>
2024-04-09 10:26:41 -0500

    Using substeps

--- games/collision-resolution/scenes/MainScene.js
+++ games/collision-resolution/scenes/MainScene.js
@@ -3,23 +3,38 @@ import "../components/KeyboardComponent.js"
 import "../components/PointConstraintsComponentTrivial.js"
 import "../components/SaveTransform.js"
 import "../components/CheckTransform.js"
+import "../components/CheckTransform2.js"
 
 const scale = 250;
 const margin = 50
 
 
-class PointGameObject extends GameObject{
-  constructor(){
+class PointGameObject extends GameObject {
+  constructor() {
     super();
     this.addComponent(new Point())
-    this.addComponent(new SaveTransform())
-    this.addComponent(new KeyboardComponent())
-    // this.addComponent(new PointConstraintsComponentTrival())
-    this.addComponent(new CheckTransform())
+
+    if (false) {
+      this.addComponent(new KeyboardComponent())
+    }
+    if (false) {
+      this.addComponent(new SaveTransform())
+      this.addComponent(new KeyboardComponent())
+      this.addComponent(new CheckTransform())
+    }
+    if(false){
+      this.addComponent(new KeyboardComponent())
+      this.addComponent(new PointConstraintsComponentTrival())
+    }
+    if (true) {
+      this.addComponent(new SaveTransform())
+      this.addComponent(new KeyboardComponent())
+      this.addComponent(new CheckTransform2())
+    }
   }
-  start(ctx){
-    this.transform.x = scale+margin-margin*2
-    this.transform.y = scale+margin-margin*2
+  start(ctx) {
+    this.transform.x = scale + margin - margin * 2
+    this.transform.y = scale + margin - margin * 2
     super.start()
 
   }
@@ -37,14 +52,14 @@ class MainScene extends Scene {
 
 
   start(ctx) {
-   
+
 
     GameObject.instantiate(new WallGameObject(), margin, scale + margin, margin * 2, scale * 2 - margin * 2)
     GameObject.instantiate(new WallGameObject(), scale * 2 + margin, scale + margin, margin * 2, scale * 2 - margin * 2)
     GameObject.instantiate(new WallGameObject(), scale + margin, margin, scale * 2 - margin * 2, margin * 2)
     GameObject.instantiate(new WallGameObject(), scale + margin, scale * 2 + margin, scale * 2 - margin * 2, margin * 2)
-    
-    
+
+
     GameObject.instantiate(new WallGameObject(), scale + margin, scale + margin, margin * 4, margin * 2)
     GameObject.instantiate(new WallGameObject(), scale + margin, scale + margin, margin * 2, margin * 4)
 
